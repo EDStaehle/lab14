3398c680b73cd36721d44184ba0075709288a818
Dennis Nichols <dgnichols22@gmail.com>
2022-09-29 15:52:23 -0500

    filled out app.js functions

--- js/app.js
+++ js/app.js
@@ -9,16 +9,23 @@ const Cart = function(items) {
 Cart.prototype.addItem = function(product, quantity) {
   // TODO: Fill in this instance method to create a new CartItem and add it to this.items
   let newItem = new CartItem(product, quantity);
+  this.items.push(newItem);
   
 };
 
 Cart.prototype.saveToLocalStorage = function() {
   // TODO: Fill in this instance method to save the contents of the cart to localStorage
+  localStorage.setItem('cart', JSON.stringify(this.items));
 };
 
 Cart.prototype.removeItem = function(item) {
   // TODO: Fill in this instance method to remove one item from the cart.
   // Note: You will have to decide what kind of parameter to pass in here!
+  for (let thing in this.items) {
+    if (thing === item) {
+      this.items.splice(this.items[thing],1);
+    }
+  }
 };
 
 const CartItem = function(product, quantity) {
